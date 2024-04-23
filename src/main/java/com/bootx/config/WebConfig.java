eece35556a62cd88971822951c0d2a781767639f
black <1169794338@qq.com>
2024-04-23 15:33:57 +0800

    init

--- src/main/java/com/bootx/config/WebConfig.java
+++ src/main/java/com/bootx/config/WebConfig.java
@@ -10,6 +10,7 @@ import com.bootx.security.CurrentUserMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -52,6 +53,16 @@ public class WebConfig implements WebMvcConfigurer {
         CurrentUserHandlerInterceptor currentUserHandlerInterceptor = new CurrentUserHandlerInterceptor();
         currentUserHandlerInterceptor.setUserClass(Admin.class);
         return currentUserHandlerInterceptor;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
     }
 
     @Override
